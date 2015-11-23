096aa4f1c1428682811927296edd4a4b0997324b
1200974 <andrewduncan777@btinternet.com>
2015-11-23 14:41:13 +0000

    added server.js file

--- retrieve.js
+++ retrieve.js
@@ -36,13 +36,16 @@ http.createServer(function(request, response) {
                 if (err) {
                     response.write(err);
                 } else {
-                    response.write("Fetched: " + result.name + " : " + result.age + " : " + result.roles.toString() +'\n');
+                    response.write('Fetched: ' + result.name + " : " + result.age + " : " + result.roles.toString() +'\n');
                 }
                 //if the result is null, there are no more results, it’s ok to close everything
                 if (result == null) {
                     response.end('Completed');
                     db.close();
                 }
+
+                db.close();
+                response.end('Finished, Connection closed \n');
             });
         }
     });
